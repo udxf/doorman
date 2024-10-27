d544794b0a8485b833b00e7c157df944c2424894
udxf <127219377+udxf@users.noreply.github.com>
2024-10-28 00:17:14 +0100

    Add /revise command
    Closes #7

---
+++ commands/revise.js
@@ -0,0 +1,60 @@
+const { roleMention, userMention, EmbedBuilder } = require('discord.js')
+const { history, session } = require('../util/database')
+
+/** @type {import('discord.js').ApplicationCommand} */
+module.exports = {
+	name: 'revise',
+	description: 'See who is allowed to join channel',
+	dmPermission: false,
+
+	// TODO: maybe add an option to check someone else's channel
+
+	/**
+	 * @this {import('discord.js').Interaction}
+	 * @param {import('discord.js').BaseGuildVoiceChannel} [channel=this.member.voice.channel] 
+	 */
+	async execute (channel = this.member.voice.channel) {
+
+		const { hub, host } = Object(await session.get(channel?.id))
+
+		if (!channel || !host)
+			return this.reply({
+				ephemeral: true,
+				content: 'Please join a temporary voice channel before using this command.'
+			})
+
+		const { permissions = [] } = await history.get({ hub, user: host }) || {}
+
+		const isPublic = channel
+			.permissionsFor(this.guild.roles.everyone)
+			.has('ViewChannel')
+
+		const exceptions = new Array()
+		const opposite = !isPublic ? 'allow' : 'deny'
+
+		for (let id in permissions) {
+			if (!(opposite in permissions[id])) continue
+
+			const mention = permissions[id].type
+				? userMention(id)
+				: roleMention(id)
+
+			exceptions.push(mention)
+		}
+
+		const embed = new EmbedBuilder()
+
+		embed.setTitle(`${channel} is __${ isPublic ? 'open' : 'closed' }__ for everyone ${exceptions.length ? 'except:' : ''}`)
+		embed.setColor(0x5865f2)
+
+		if (exceptions.length) embed.setDescription(exceptions.map(i => `- ${i}`).join('\n'))
+
+		if (!isPublic) embed.setFooter({ text: 'Note \u2014 Administrators always have full access!'})
+
+		this.reply({
+			ephemeral: true,
+			embeds: [embed]
+		})
+	}
+}
+
